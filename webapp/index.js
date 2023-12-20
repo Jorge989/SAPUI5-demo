b3a8a0eb4eebebd877a0e15a115aa86c25f3c9ad
Jorge <attiejorge89@gmail.com>
2023-12-20 11:25:21 -0300

    up

--- webapp/index.js
+++ webapp/index.js
@@ -1,8 +1,11 @@
-sap.ui.define(["sap/m/Text"], function (Text) {
-  "use strict";
-  XMLView.create({
-    viewName: sap.ui.demo.walkthrough.viewApp,
-  }).then(function (oView) {
-    oView.placeAt("content");
-  });
-});
+sap.ui.require(
+  ["sap/ui/core/mvc/XMLView", "sap/m/Text"],
+  function (XMLView, Text) {
+    "use strict";
+    XMLView.create({
+      viewName: "sap.ui.demo.walkthrough.viewApp",
+    }).then(function (oView) {
+      oView.placeAt("content");
+    });
+  }
+);
